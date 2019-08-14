9608715a5ba9be0f9a6e83010c23237f439e2122
Jeremy Larkin <jeremy.larkin@segment.com>
2019-08-14 13:16:06 -0700

    setup git right prompt

--- fish/config.fish
+++ fish/config.fish
@@ -40,6 +40,31 @@ function fish_prompt
 	set_color normal
 end
 
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_color_branch white
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate ""
+set -g __fish_git_prompt_char_dirtystate "+"
+set -g __fish_git_prompt_char_untrackedfiles "*"
+set -g __fish_git_prompt_char_conflictedstate "x"
+set -g __fish_git_prompt_char_cleanstate ""
+
+set -g __fish_git_prompt_color_dirtystate green
+set -g __fish_git_prompt_color_stagedstate cyan
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles white
+set -g __fish_git_prompt_color_cleanstate green
+
+function fish_right_prompt
+  __fish_git_prompt
+end
+
 function fish_mode_prompt
 end
 
